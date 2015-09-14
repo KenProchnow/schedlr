e39902f3893462b5c65cc2af76fed9aa56b0090b
John Skilbeck <john.skilbeck@gmail.com>
2015-09-14 16:46:31 -0700

    got it to work on raw sql files

--- query.js
+++ query.js
@@ -1,36 +1,28 @@
 var 
-	fs 					= require('fs'),
-	path 				= require('path'),
-	sql 				= require('mssql'),
-	fileDir 		= 'SQL Files',
-	outDir 			= 'CSVs',
-	file 				= 'query2',
-	csv 				= require('fast-csv'),
-	// csvStream  	= cs
-	config 			= require('./lib/config/config.js'),
-	// csvStream 	= csv.c;reateWriteStream({headers: true}),
-	outFile			= fs.createWriteStream(path.join(outDir, file + '.csv')),
-	data 				= '';
+	fs 					= require('fs'), 				path = require('path'),
+	csv 				= require('fast-csv'), 	config 			= require('./lib/config/config.js'),
+	sql 				= require('mssql'),			data 				= '';
+	file 				= 'query2',							fileDir 		= 'SQL Files', 			
+	outDir 			= 'CSVs', 							outFile			= fs.createWriteStream(path.join(outDir, file + '.csv')),
 	sqlFile 		= fs.createReadStream(path.join(fileDir, file + '.sql'))
 	;
 
 sqlFile.on('data', function(chunk){ data+=chunk; });
 
 sqlFile.on('end', function() {
-	runQueryFromFile(data);
+	cleanUp(data);
 });
 
-var runQueryFromFile = function(sql){
-    sql.replace(/(\r\n|\n|\r)/gm," "); // remove newlines
-    sql.replace(/\s+/g, ' '); // excess white space
-    runQuery(sql);
+var cleanUp = function(sql){
+  sql.replace(/(\r\n|\n|\r)/gm," "); // remove newlines
+  sql.replace(/\s+/g, ' '); // excess white space
+  runQuery(sql);
 };
 
 var runQuery = function(statement) {
 	var connection 	= new sql.Connection(config, function(err) {
 		var r = new sql.Request(connection);
 		r.query(statement, function(err, results){
-			// console.log(results);
 			saveCSV2(results);
 		});
 	});
@@ -42,24 +34,15 @@ var runQuery = function(statement) {
 
 
 var saveCSV = function(data) {
-	// console.log(data);
-
 	csv.stringify(data, function(err,data){
 		outFile.write(data);
 		outFile.end();
 	});
-
-
 };
 
 var saveCSV2 = function(data) {
 	csv.write(data, {headers: true}).pipe(outFile);
 };
-
-
-
-
-
 
 
 

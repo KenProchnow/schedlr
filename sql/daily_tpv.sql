3e996c08c504728b8c5476d1ef17a39df1788d80
John Skilbeck <john.skilbeck@gmail.com>
2015-10-16 18:12:18 -0700

    safely escaped FX rates in daily volume queries

--- sql/daily_tpv.sql
+++ sql/daily_tpv.sql
@@ -15,32 +15,72 @@ set @lyEnd_YTD = dateadd(yy,-1, @end)
  
  
 
+if object_id('tempdb..#CurrencyCodes') is not null drop table #CurrencyCodes
+select Currency.CharCode, Currency.CurrencyId into #CurrencyCodes
+from YapstoneDM..Currency
+group by Currency.CharCode, Currency.CurrencyId
+  
+if object_id('tempdb..#Dates') is not null drop table #Dates
+select * into #Dates from (
+       select txn.postDate_R Date, c.CharCode, c.CurrencyId 
+       from   YapstoneDM..[Transaction] txn
+       cross join (
+               select Currency.CharCode, Currency.CurrencyId
+               from YapstoneDM..Currency
+               group by Currency.CharCode, Currency.CurrencyId
+       ) c
+       where  txn.PostDate_R between @lyStart_YTD and @end
+       group by txn.PostDate_R , c.CharCode, c.CurrencyId
+) src
+ 
+-- FX Rates
 if object_id('tempdb..#EURUSD') is not null                drop table #EURUSD
-select fx.Date, fx.ExchangeRate                                           into #EURUSD                 
+select fx.Date, fx.ExchangeRate                                           into #EURUSD                
 from YapstoneDM..CurrencyExchangeRate fx
 where  BaseCurrencyId in (1) and fx.CounterCurrencyId in (3)
-     and (   fx.Date between @startYTD and @end or
-             fx.Date between @lyStart_YTD and @lyEnd_YTD )
+     and (   fx.Date between @lyStart_YTD and @end  )
 group by fx.Date, fx.ExchangeRate
+if object_id('tempdb..#RatesPREPROCESSED') is not null                 drop table #RatesPREPROCESSED
+select * into #RatesPREPROCESSED from (
+     select isnull(fx.Date,d.Date) Date, isnull(Currency.CharCode,d.CharCode) CharCode, isnull(Currency.CurrencyId,d.CurrencyId) CurrencyId,
+                      1 / ( fx.ExchangeRate / usd.ExchangeRate ) Rate
+     from #Dates d left join YapstoneDM..CurrencyExchangeRate fx  on d.Date = fx.Date
+               left join YapstoneDM..Currency on fx.CounterCurrencyId = Currency.CurrencyId
+               left join #EURUSD usd on fx.Date = usd.Date
+     where  (   d.Date between @lyStart_YTD and @end )
+     group by  isnull(fx.Date,d.Date) , isnull(Currency.CharCode,d.CharCode) , isnull(Currency.CurrencyId,d.CurrencyId)
+                      , 1 / ( fx.ExchangeRate / usd.ExchangeRate )
+     union
+       select isnull(fx.Date,d.Date) Date, d.CharCode, d.CurrencyId ,fx.ExchangeRate                     
+       from   #Dates d left join YapstoneDM..CurrencyExchangeRate fx  on d.Date = fx.Date and d.CurrencyId = fx.BaseCurrencyId
+               and BaseCurrencyId in (1) and fx.CounterCurrencyId in (3)
+               left join YapstoneDM..Currency on fx.CounterCurrencyId = Currency.CurrencyId
+       where  d.CurrencyId in (1)
+               and  (   d.Date between @lyStart_YTD and @end  )
+       group by isnull(fx.Date,d.Date) , d.CharCode, d.CurrencyId, fx.ExchangeRate
+) src
+order by Date asc
+ 
+ 
+if object_id('tempdb..#FallbackRates') is not null drop table #FallBackRates
+select * into #FallbackRates from (
+       select 'EUR' CharCode, 1.35 as Rate union select 'GBP', 1.6 union select 'CAD', 0.9 union select 'USD', 1
+) src
  
 if object_id('tempdb..#Rates') is not null                 drop table #Rates
-select * into #Rates from (
-     select fx.Date, Currency.CharCode, Currency.CurrencyId, 1 / ( fx.ExchangeRate / usd.ExchangeRate ) Rate
-             
-     from YapstoneDM..CurrencyExchangeRate fx  join YapstoneDM..Currency on fx.CounterCurrencyId = Currency.CurrencyId
-                left join #EURUSD usd on fx.Date = usd.Date
-     where
-             (   fx.Date between @startYTD and @end or
-             fx.Date between @lyStart_YTD and @lyEnd_YTD )
-     group by fx.Date, Currency.CharCode, Currency.CurrencyId, 1 / ( fx.ExchangeRate / usd.ExchangeRate )
-     union
-     select fx.Date, Currency.CharCode, Currency.CurrencyId, fx.ExchangeRate                      
-     from YapstoneDM..CurrencyExchangeRate fx  join YapstoneDM..Currency on fx.BaseCurrencyId = Currency.CurrencyId
-     where  BaseCurrencyId in (1) and fx.CounterCurrencyId in (3)  
-             and  (   fx.Date between @startYTD and @end or
-             fx.Date between @lyStart_YTD and @lyEnd_YTD )
-     group by fx.Date, Currency.CharCode, Currency.CurrencyId, fx.ExchangeRate
-) src
+select Date,src.CharCode,CurrencyId,isnull(src.Rate,fallback.Rate) Rate  into #Rates from (
+       select Date,CharCode,CurrencyId,(
+               select top 1 Rate
+               from   #RatesPREPROCESSED r1
+               where  r1.Date <= r.Date
+                              and r1.Rate is not null
+                              and r1.CharCode = r.CharCode
+               order by r1.Date desc
+       ) Rate
+       from #RatesPREPROCESSED r
+) src left join #FallBackRates fallback on fallback.CharCode = src.CharCode
+ 
+
  
 if object_id('tempdb..#Yesterday') is not null drop table #Yesterday
 select

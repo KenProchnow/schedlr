c4edd3ec2f59a3a9682cff4df0fb145b4558df1e
John Skilbeck <john.skilbeck@gmail.com>
2015-10-28 16:57:14 -0700

    Added two jobs

---
+++ jobs/Rent/PenetrationRate.js
@@ -0,0 +1,16 @@
+var 
+  parse = require('./../../parse.js').parseSQL,
+  query = require('./../../query.js').executeQuery,
+  format = require('./../../lib/format.js').cleanData,
+  html = false, // send results as html?
+  folder = 'Rent',
+  file = 'PenetrationRate'
+  ; 
+
+if (html) { var email = require('./../../email.js').email; } else { var email = require('./../../lib/email_library/email.js').email; }
+ 
+parse(folder, file, function(sql){
+  query(sql, folder, file, function(data, folder, file){
+    email(data, folder, file);  
+  });
+});
